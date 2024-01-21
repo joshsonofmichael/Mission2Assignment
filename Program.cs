05355f68abf829418cbaaf1eca0870764e158eff
joshuamichaelson <joshuamichaelson@JOSHUAMICHA7B44>
2024-01-21 12:37:45 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//using Mission2Assignment;
+
+
+using Mission2Assignment;
+
+Console.WriteLine("Welcome to the dice throwing simulator!");
+Console.WriteLine("How many dice rolls would you like to simulate?");
+string input = Console.ReadLine();
+int numRolls = int.Parse(input);
+
+//Console.WriteLine(numRolls);
+Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+Console.WriteLine("Each '*' represents 1% of the total number of rolls");
+Console.WriteLine("Total number of rows = " + numRolls);
+
+RollDice rollDice = new RollDice();
+
+int[] results = rollDice.Roll(numRolls);
+for (int i = 0; i < results.Length; i++)
+{
+    float percent = ((float)results[i] / numRolls) * 100;
+    int roundedPercent = (int)Math.Round(percent);
+    string numStars = String.Concat(Enumerable.Repeat("*", roundedPercent));
+    if (i < 8)
+    {
+        numStars = " " + numStars;
+    }
+
+    Console.WriteLine((i + 2) + ": " + numStars);
+}
+
+Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
---
+++ RollDice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment
+{
+    internal class RollDice
+    {
+        // Create a new instance of the Random class
+        Random random = new Random();
+        private int OneRoll()
+        {
+            
+
+            // Generate a random number between 1 and 6 (inclusive)
+            int diceRoll = random.Next(1, 7);
+            
+            return diceRoll;
+        }
+        public int[] Roll(int numRolls)
+        {
+            int[] combinations = new int[11];
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                int dice1 = OneRoll();
+                int dice2 = OneRoll();
+                int combination = dice1 + dice2;
+
+                combinations[(combination - 2)]++;
+            }
+
+            return combinations;
+        }
+    }
+}
